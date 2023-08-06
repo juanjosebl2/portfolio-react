db26aa5001996d2f556cedd5fbbbeb6d51973394
juanjosebl2 <juanjosebl2@gmail.com>
2023-08-06 11:25:26 +0200

    added live of movies

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -16,7 +16,7 @@ const projects = [
         "image_url": "projects/movie-react.png",
         "description": "A Frontend web application, you can see the movies actually and you can search movies, if you press a film, you will see details of these movie",
         "source_link": "https://github.com/juanjosebl2/movie-react",
-        "live_link": "N/A",
+        "live_link": "https://movie-juan-web.netlify.app/",
         "tech_stack": [
             "React",
             "Next.js",
