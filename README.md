3962e7d4fb8df88437b878feec15621b3c0ac255
juanjosebl2 <juanjosebl2@gmail.com>
2023-08-10 13:58:42 +0200

    added more proyects

--- README.md
+++ README.md
@@ -68,3 +68,12 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/d
 ### `npm run build` fails to minify
 
 This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+
+
+INSTALL
+
+git clone https://github.com/juanjosebl2/portfolio-react.git
+cd portfolio-react
+
+npm install
+npm start
--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -19,10 +19,51 @@ const projects = [
         "live_link": "https://movie-juan-web.netlify.app/",
         "tech_stack": [
             "React",
-            "Next.js",
+            "JavaScript",
             "HTML",
             "CSS",
             "Api"
+        ]
+    },
+    {
+        "project_name": "Crud Spring Boot",
+        "image_url": "projects/springBoot-backend.png",
+        "description": "I realized my first proof of Spring Boot of JAVA for backend, crud of movies and i use data base MongoDB",
+        "source_link": "https://github.com/juanjosebl2/springBoot-MongoDB",
+        "live_link": "N/A",
+        "tech_stack": [
+            "Java",
+            "Spring Boot",
+            "MongoDB",
+        ]
+    },
+    {
+        "project_name": "Roles and Crud of users, proyects, exercise and incidents",
+        "image_url": "projects/data-sheet-laravel.png",
+        "description": "Firsts projects with laravel full stack, role-based project where you can do a crud with users, projects, exercise and incidents for a data sheet, old project not updated ",
+        "source_link": "https://github.com/juanjosebl2/laravel-fichaTecnica",
+        "live_link": "N/A",
+        "tech_stack": [
+            "Laravel",
+            "Blade",
+            "PHP",
+            "JavaScript",
+            "CSS",
+            "mysql"
+        ]
+    },
+    {
+        "project_name": "PortFolio for presentation me",
+        "image_url": "projects/portfolio.png",
+        "description": "First projects with react for basic use, with what I was learning with react, html, css I started to make my portfolio",
+        "source_link": "https://github.com/juanjosebl2/portfolio-react",
+        "live_link": "https://portfolio-juan-web.netlify.app/home",
+        "tech_stack": [
+            "React",
+            "JavaScript",
+            "HTML",
+            "CSS",
+            "JSON"
         ]
     }
 ]
